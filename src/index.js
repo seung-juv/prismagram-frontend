0ca4d04cfa679324cccae3f25cfa6fb04c99e611
seungju <kingsky32@gmail.com>
2020-07-16 04:42:17 +0900

    prismagram-frontend

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 import App from "./Components/App";
 import { ApolloProvider } from "react-apollo-hooks";
 import Client from "./Apollo/Client";
-import saga from "./Saga";
-import { configureStore } from "./Store";
-
-const store = configureStore();
-store.runSaga(saga);
 
 ReactDOM.render(
   <ApolloProvider client={Client}>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </ApolloProvider>,
   document.getElementById("root")
 );
