125a7e2c4f57693b94f4d61d808ab3de3ce33943
seungju <kingsky32@gmail.com>
2020-10-05 14:02:07 +0900

    Update URI

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,7 +2,7 @@ import ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
 
 export default new ApolloClient({
-  uri: "http://kingsky32.co.kr:4000",
+  uri: "http://prismagram-backend.seung-ju.com",
   clientState: {
     defaults,
     resolvers
