9bd2be12275a0b67b2f5a9fa54e85b60b62a700b
kingsky32 <kingsky32@gmail.com>
2021-02-11 17:52:25 +0900

    rename

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,12 +2,12 @@ import ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
 
 export default new ApolloClient({
-  uri: "http://prismagram-backend.seung-ju.com",
+  uri: "http://prismagram-server.seung-ju.com",
   clientState: {
     defaults,
-    resolvers
+    resolvers,
   },
   headers: {
-    Authorization: `Bearer ${localStorage.getItem("token")}`
-  }
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  },
 });
